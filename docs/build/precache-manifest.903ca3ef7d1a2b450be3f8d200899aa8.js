d8fa53ad3df03468fe10f732885be7c373a56fdc
Alexander Letnikow <reclamme@gmail.com>
2020-03-07 09:03:41 +0200

    Add build

---
+++ docs/build/precache-manifest.903ca3ef7d1a2b450be3f8d200899aa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53fb14cc79c55631f02f0edb5632e49",
+    "url": "https://horat1us.github.io/react-svg-circle-graph/index.html"
+  },
+  {
+    "revision": "a5ddba1a496311e05ef8",
+    "url": "https://horat1us.github.io/react-svg-circle-graph/static/js/2.cbf12c96.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "https://horat1us.github.io/react-svg-circle-graph/static/js/2.cbf12c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde678445c9f39925630",
+    "url": "https://horat1us.github.io/react-svg-circle-graph/static/js/main.b98c7bd9.chunk.js"
+  },
+  {
+    "revision": "132bd25ba789fe8275d6",
+    "url": "https://horat1us.github.io/react-svg-circle-graph/static/js/runtime-main.3577a467.js"
+  }
+]);
---
+++ docs/precache-manifest.9a4a9ebdbd735a3ab0f2cc1a290aaf08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a3239444ce4af68ff463783be36d8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5ddba1a496311e05ef8",
+    "url": "/static/js/2.cbf12c96.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.cbf12c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde678445c9f39925630",
+    "url": "/static/js/main.b98c7bd9.chunk.js"
+  },
+  {
+    "revision": "77c6b0d79eab43389060",
+    "url": "/static/js/runtime-main.4796e7fc.js"
+  }
+]);
